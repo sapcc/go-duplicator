44456085a9cad33b5f5ac4300c3ac44f14488816
Chuan Miao <>
2018-12-21 12:28:03 +0100

    cleanup log message in pipebuffer.go

--- pipebuffer.go
+++ pipebuffer.go
@@ -3,7 +3,6 @@ package main
 import (
 	"bytes"
 	"errors"
-	"fmt"
 	"io"
 	"log"
 	"sync"
@@ -65,10 +64,6 @@ func (m *pipeBuffer) Stop() {
 // is closed. The return error is ErrBufferStopped when the buffer is
 // blocked.
 func (m *pipeBuffer) Write(p []byte) (int, error) {
-	log := func(c interface{}) { log.Printf("pipeBuffer write: %s", c) }
-	log("start")
-	log(string(p))
-
 	select {
 	case <-m.done:
 		return 0, ErrBufferClosed
@@ -95,17 +90,12 @@ func (m *pipeBuffer) empty() bool {
 }
 
 func (m *pipeBuffer) Read(p []byte) (int, error) {
-	log := func(c interface{}) { log.Printf("pipeBuffer read: %s", c) }
-	log("start")
-
 	// Read buffer is empty
 	if m.buf[m.rd].Len() == 0 {
-		log("m.rd is empty")
 		select {
 		case <-m.done:
 			return 0, ErrBufferClosed
 		case <-m.stop:
-			log("buffer stopped")
 			// write is blocked, drain the buffer
 			m.mtx.Lock()
 			defer m.mtx.Unlock()
@@ -115,7 +105,6 @@ func (m *pipeBuffer) Read(p []byte) (int, error) {
 			m.rd, m.wr = m.wr, m.rd
 			return 0, nil
 		case <-m.swap:
-			log("swap read/write")
 			// block read until write buffer is not empty
 			m.mtx.Lock()
 			m.rd, m.wr = m.wr, m.rd
@@ -131,80 +120,7 @@ func (m *pipeBuffer) Read(p []byte) (int, error) {
 	default:
 		n, _ := m.buf[m.rd].Read(p)
 		time.Sleep(500 * time.Millisecond)
-		log(fmt.Sprintf("%d bytes", n))
+		log.Printf("%d bytes read", n)
 		return n, nil
 	}
 }
-
-// // Unblock buffer
-// func (m *pipeBuffer) UnBlock() error {
-// 	select {
-// 	case <-m.done:
-// 		return ErrBufferClosed
-// 	default:
-// 		m.blocked = false
-// 		return nil
-// 	}
-// }
-
-// // Block buffer
-// func (m *pipeBuffer) Block() (err error) {
-// 	select {
-// 	case <-m.done:
-// 		return ErrBufferClosed
-// 	default:
-// 		m.blocked = true
-// 		return nil
-// 	}
-// }
-
-// WriteTo
-// Write buffer A to Writer w, if buffer A is not emptpy
-// Otherwise wait either buffer B is not empyt or kill signal
-// func (m *pipeBuffer) WriteTo(w io.Writer) (int64, error) {
-// 	log.Print("writeto")
-// 	if m.buf[m.rd].Len() == 0 {
-// 		select {
-// 		case <-m.done:
-// 			return 0, ErrBufferClosed
-// 		case <-m.swap:
-// 			m.mtx.Lock()
-// 			m.rd, m.wr = m.wr, m.rd
-// 			m.mtx.Unlock()
-// 		}
-// 	}
-// 	return m.buf[m.rd].WriteTo(w)
-// }
-
-// func (m *pipeBuffer) readFromAll(p []byte) (n int) {
-// 	m.mtx.Lock()
-// 	defer m.mtx.Unlock()
-// 	var lp = p[:]
-
-// 	for i := 0; i < 2; i++ {
-// 		written, _ := m.buf[m.rd].Read(lp)
-// 		n += written
-// 		lp = p[n:]
-// 		if m.buf[m.rd].Len() != 0 {
-// 			return
-// 		}
-// 		m.rd, m.wr = m.wr, m.rd
-// 	}
-// 	return
-// }
-
-// func (m *pipeBuffer) Flush(w io.Writer) (int64, error) {
-// 	m.mtx.Lock()
-// 	defer m.mtx.Unlock()
-// 	var n1, n2 int64
-// 	if n1, err := m.buf[m.wr].WriteTo(w); err != nil {
-// 		return n1, err
-// 	}
-// 	if n2, err := m.buf[m.rd].WriteTo(w); err != nil {
-// 		return n1 + n2, err
-// 	}
-// 	return n1 + n2, nil
-// }
-
-// ErrBufferEmpty means buffer A and B are both empty
-// var ErrBufferEmpty = errors.New("Buffer Empty")
